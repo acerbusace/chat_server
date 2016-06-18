89628e13a8fda5da80745baa13e0fc725412dba4
Alex Patel <acerbusace@localhost.localdomain>
2016-06-18 11:23:24 -0400

    changed tabs of 4 to 2

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    console.log('user connected');
+  console.log('user connected');
 
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 
-    socket.on('chat message', function(msg){
-        console.log('chat message: ', msg);
-        io.emit('chat message', msg);
-    });
+  socket.on('chat message', function(msg){
+    console.log('chat message: ', msg);
+    io.emit('chat message', msg);
+  });
 });
 
 var port = 8000;
 http.listen(port, function(){
-    console.log('listenting on port ' + port);
+  console.log('listenting on port ' + port);
 });
 
 /*
@@ -32,37 +32,36 @@ const net = require('net')
 sockets = []
 
 var server = net.createServer(function(socket){
-    sockets.push(socket);
-    console.log('user (' + socket.remoteAddress + ' | ' + socket.remotePort + ') has connected...');
-    socket.write('\nwelcome to the chat server!\n\n')
-    socket.write('please enter your username: ')
+  sockets.push(socket);
+  console.log('user (' + socket.remoteAddress + ' | ' + socket.remotePort + ') has connected...');
+  socket.write('\nwelcome to the chat server!\n\n')
+  socket.write('please enter your username: ')
 
-    socket.on('data', function(data){
-        var message = String(data)
+  socket.on('data', function(data){
+    var message = String(data)
 
-        if (socket.name === undefined){
-            socket.name = message.trim('\n')
-            send(socket.name + ' has connected to the chat server...\n', socket)
-            socket.write('\n')
-        } else{
-            send(socket.name + ': ' + message, socket);
-        }
-    });
+    if (socket.name === undefined){
+      socket.name = message.trim('\n')
+      send(socket.name + ' has connected to the chat server...\n', socket)
+      socket.write('\n')
+    } else{
+      send(socket.name + ': ' + message, socket);
+    }
+  });
 
-    socket.on('end', function(){
-        sockets.splice(sockets.indexOf(socket), 1);
-        send(socket.name + ' has disconnected from the chat server...\n')
-        console.log('user (' + socket.remoteAddress + ' | ' + socket.remotePort + ') has disconnected...');
-    });
+  socket.on('end', function(){
+    sockets.splice(sockets.indexOf(socket), 1);
+    send(socket.name + ' has disconnected from the chat server...\n')
+    console.log('user (' + socket.remoteAddress + ' | ' + socket.remotePort + ') has disconnected...');
+  });
 });
 
 function send(message, socket=null){
-    for (i = 0; i < sockets.length; ++i){
-        if (sockets[i] !== socket && socket.name !== undefined)
-            sockets[i].write(message);
-    }
+  for (i = 0; i < sockets.length; ++i){
+    if (sockets[i] !== socket && socket.name !== undefined)
+      sockets[i].write(message);
+  }
 }
-
 
 server.listen(8000);
 */
