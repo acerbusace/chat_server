927f2b5579df6a7e101b5d1b8dc7593ae94bbfc7
Alex Patel <acerbusace@localhost.localdomain>
2016-06-18 10:12:54 -0400

    base version

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello World!</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        console.log('chat message: ', msg);
+        io.emit('chat message', msg);
+    });
 });
 
 var port = 8000;
