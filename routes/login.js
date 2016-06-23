4ced9e75f48fc858a2f797231f14a9fcd03e1687
Kunall Banerjee <yeskunall96@gmail.com>
2016-06-23 06:39:32 -0400

    Fix some code

--- routes/login.js
+++ routes/login.js
@@ -4,5 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login', (req, res) => {
-  res.sendFile('login.html', { root: 'views' });
+    res.sendFile('login.html', {
+        root: 'views'
+    });
 });
+
+module.exports = router;
