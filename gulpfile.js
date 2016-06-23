0e364062cbdcdd9b676df4e70428683d4b82d968
Kunall Banerjee <yeskunall96@gmail.com>
2016-06-23 11:01:06 -0400

    Add gulp-task:nodemon|sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 /* jshint esversion: 6 */
 
 const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon');
 const sass = require('gulp-sass');
 
-gulp.task('watch', function() {
+gulp.task('watch', ['sync'], function() {
     // watch for SCSS files
     gulp.watch('./public/stylesheets/**/*.scss', ['scss']);
 });
@@ -13,5 +15,31 @@ gulp.task('scss', function() {
         .pipe(sass({
             outputStyle: 'compressed'
         })).on('error', sass.logError)
-        .pipe(gulp.dest('./public/stylesheets/min'));
+        .pipe(gulp.dest('./public/stylesheets/min'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('sync', ['nodemon'], function() {
+    browserSync.init({
+        proxy: 'http://localhost:8000',
+    });
+});
+
+gulp.task('nodemon', function(cb) {
+    var started = false;
+
+    return nodemon({
+        env: {
+            'NODE_ENV': 'development'
+        },
+        script: 'server.js'
+    }).on('start', function() {
+        // to avoid nodemon being started multiple times
+        if (!started) {
+            cb();
+            started = true;
+        }
+    });
 });
