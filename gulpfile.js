6961672eaf26a4a42a28a47ffbf790287aa4b700
Kunall Banerjee <yeskunall96@gmail.com>
2016-06-23 09:44:55 -0400

    Add gulp-sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 /* jshint esversion: 6 */
 
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('hello', function() {
-    console.log('Hello from Gulp!');
+gulp.task('scss', function() {
+    return gulp.src('./public/stylesheets/app.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        })).on('error', sass.logError)
+        .pipe(gulp.dest('./public/stylesheets/min'));
 });
