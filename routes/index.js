f1c95cf9d7aa228b43a03b471f65f95d280f535c
Alex Patel <acerbusace@localhost.localdomain>
2016-06-23 17:36:59 -0400

    removed package

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+/* jshint esversion: 6 */
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile('index.html', {
+        root: 'views'
+    });
+});
+
+module.exports = router;
