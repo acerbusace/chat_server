3b2c0e5fb54f20c8bd6450f7d6be5b35c193f23c
Alexkumar Patel <acerbus.ace@gmail.com>
2016-06-26 16:52:33 -0400

    added code to test mongo.js

--- server.js
+++ server.js
@@ -9,25 +9,42 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const path = require('path');
+
 const index = require('./routes/index');
 const login = require('./routes/login');
 const authjs = require('./lib/auth.js');
 const auth = new authjs({success:'/', failure:'/login'});
-const mongojs = require('./lib/mongo.js');
-// const mongo = new mongojs();
-const path = require('path');
+const dbHandler = require('./lib/mongo.js');
+
+dbHandler(function(mongo) {
+  // mongo.createUser({
+  //   username: 'hello',
+  //   password: 'world'
+  // });
+  
+  // mongo.findUser('hello');
+});
+
+// mongo.createUser({
+//   username: 'hello',
+//   password: 'world'
+// });
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', login);
+
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
+
 app.use(session({
-    secret: 'this is a legit app',
-    resave: false,
-    saveUninitialized: false
+  secret: 'this is a legit app',
+  resave: false,
+  saveUninitialized: false,
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.post('/login', auth.auth);
@@ -40,21 +57,21 @@ app.set('view engine', 'jade');
 /* ----------------------------------------------------------------------------- */
 
 io.on('connection', function(socket) {
-    console.log('user connected');
+  console.log('user connected');
 
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 
-    socket.on('chat message', function(msg) {
-        console.log('chat message: ', msg);
-        io.emit('chat message', msg);
-    });
+  socket.on('chat message', function(msg) {
+    console.log('chat message: ', msg);
+    io.emit('chat message', msg);
+  });
 });
 
 var port = 8000;
 http.listen(port, function() {
-    console.log('listening on port ' + port);
+  console.log('listening on port ' + port);
 });
 
 module.exports = app;
