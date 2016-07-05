c5aeef3e1b9db2175ebe0785faa3a4772810283b
Kunall Banerjee <yeskunall96@gmail.com>
2016-07-05 17:54:06 -0400

    Confirm Mocha works

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+/* jshint esversion:6  */
+
+/* DEFAULT TEST
+ * It checks out!
+ */
+
+const should = require('should');
+
+describe('addition', function() {
+    'use strict';
+    it('should add 1+1 correctly', function(done) {
+        val = 2;
+        val.should.equal(2);
+        done();
+    });
+});
