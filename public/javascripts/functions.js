0784a6fcb2f4def0b8cbfabe6ae83658b3a4fe0c
Alexkumar Patel <acerbus.ace@gmail.com>
2016-07-12 23:35:19 -0400

    changed code to test groups

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,19 +1,34 @@
 $(document).ready(function() {
-    var socket = io();
-    $('form').submit(function() {
-        var message = {
-            msg: $('#m').val(),
-            created_at: Date.now(),
-        };
-        socket.emit('chat message', JSON.stringify(message));
-        $('#m').val('');
-        return false;
-    });
+  var socket = io();
+  $('form').submit(function() {
+    console.log('at submit');
+    socket.emit('get groups');
+    console.log('at get gg');
+    socket.on('get groups', function(groups) {
+      console.log('at groups');
+      console.log(groups);
+      groups = JSON.parse(groups);
 
-    socket.on('chat message', function(msg) {
-        var data = JSON.parse(msg);
-        var date = new Date(data.created_at);
-        $('#messages').append($('<li>').text(data.username + ': ' + data.msg + ' | ' + date.toLocaleTimeString()));
+      var message = {
+        msg: $('#m').val(),
+        created_at: Date.now(),
+        groupId: groups['groups'][0],
+      };
+
+      $('#m').val('');
+
+      console.log(message);
+
+      socket.emit('add message', JSON.stringify(message));
     });
+    
+    return false;
+  });
+
+  socket.on('chat message', function(msg) {
+    var data = JSON.parse(msg);
+    var date = new Date(data.created_at);
+    $('#messages').append($('<li>').text(data.userId + ': ' + data.msg + ' | ' + date.toLocaleTimeString()));
+  });
 
 });
